08aa2e3c57a361158ee2575a2f683c9201d57ac3
Marco Francisco <marco.francisco@vision-box.com>
2021-03-09 15:33:02 +0000

    [TUTORIAL] Finished props and children. Moving to clas based components

--- src/components/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './comment/comment-detail';
--- src/models/comment.ts
+++
@@ -1,9 +0,0 @@
-export class TutComment {
-    author!: string;
-    comment!: string;
-    time!: string;
-
-    constructor(item?: any) {
-        Object.assign(this, item);
-    }
-}
